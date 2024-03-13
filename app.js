c30bf7ba6e32dbdbb140cd97646f1bc6ed22e2ff
hugo de araujo <hugo.saraujo@tutanota.com>
2024-03-13 19:25:43 -0300

    Inicio do projeto sorteador de números

---
+++ app.js
@@ -0,0 +1,8 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value)
+    let min = parseInt(document.getElementById('de').value);
+    let max = parseInt(document.getElementById('ate').value);
+
+
+    console.log(`${min}, ${max}, ${quantidade}`);
+}
