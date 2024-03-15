e92b4c01ba485e8c06f552473d64b33dad1682ba
hugo de araujo <hugo.saraujo@tutanota.com>
2024-03-14 23:53:03 -0300

    Inserindo os números sorteados na tela para o usuário

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ function sortear(){
     let min = parseInt(document.getElementById('de').value);
     let max = parseInt(document.getElementById('ate').value);
 
+    let sorteados = [];
+    let numero; 
 
-    console.log(`${min}, ${max}, ${quantidade}`);
+    for(let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatório(min, max);
+        sorteados.push(numero);
+    }
+    let mensagem = document.getElementById('resultado');
+    mensagem.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`     
+}
+
+function obterNumeroAleatório(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
