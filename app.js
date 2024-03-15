422d61af7b5290288303e94e14c9e2d40d1cb3e7
hugo de araujo <hugo.saraujo@tutanota.com>
2024-03-15 18:52:03 -0300

    Habilitação do botão reiniciar e implementando a lógica do botão

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value)
     let min = parseInt(document.getElementById('de').value);
@@ -13,13 +14,39 @@ function sortear(){
             numero = obterNumeroAleatório(min, max);            
         } else {
             sorteados.push(numero);
-        }
-        
+        }   
     }
-    let mensagem = document.getElementById('resultado');
-    mensagem.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`     
+    
+    let mensagem = document.getElementById('resultado')
+    mensagem.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    alterarStatusBotao();     
 }
 
 function obterNumeroAleatório(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    
+    }
+}
+
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    alterarStatusBotao();
 }
